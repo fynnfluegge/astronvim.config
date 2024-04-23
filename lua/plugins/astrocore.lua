51578d3f3d0e38e193703d2f502146448c303446
fynnfluegge <fynnfluegge@gmx.de>
2024-04-23 23:16:12 +0200

    add options

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -28,13 +28,23 @@ return {
         relativenumber = true, -- sets vim.opt.relativenumber
         number = true, -- sets vim.opt.number
         spell = false, -- sets vim.opt.spell
-        signcolumn = "auto", -- sets vim.opt.signcolumn to auto
+        signcolumn = "yes", -- sets vim.opt.signcolumn to auto
+        foldcolumn = "1",
         wrap = false, -- sets vim.opt.wrap
+        cmdheight = 1, -- sets vim.opt.cmdheight
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
         -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
         -- This can be found in the `lua/lazy_setup.lua` file
+        mapleader = " ", -- sets vim.g.mapleader
+        autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
+        cmp_enabled = true, -- enable completion at start
+        autopairs_enabled = true, -- enable autopairs at start
+        diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
+        icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+        ui_notifications_enabled = true, -- disable notifications when toggling UI elements
+        resession_enabled = false, -- enable experimental resession.nvim session management (will be default in AstroNvim v4)
       },
     },
     -- Mappings can be configured through AstroCore as well.
